bcbbeea0c6eb09895533c9e67f698b1269e5c1e8
Apostolis Karageorgopoulos <el14178@central.ntua.gr>
2025-05-29 19:57:27 +0300

    github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.DEPLOY_TARGET === "gh-pages";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enables static HTML export
+  basePath: isGithubPages ? "/anthoulis-poetry" : "",
+  assetPrefix: isGithubPages ? "/anthoulis-poetry/" : "",
 };
 
 export default nextConfig;
